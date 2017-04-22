31c6187a155e9c620bd75c468bdd3e7ff3fa0f72
waxa <waxaman@gmail.com>
2017-04-22 18:37:35 +0200

    p2 solo con lex

---
+++ prueba.java
@@ -0,0 +1,13 @@
+public class HelloWorld {
+
+    public static void main(String[] args) {
+        System.out.println("Hello, World");
+      	int aux = 3;
+      	int aux2 = 4;
+      	String aux3 = "aux3string con espacios \n y saltos";
+      	float aux4asd = 33.2;
+      	String fallo = "public";
+    }
+
+    private boolean metodo(String asd) {}
+}
